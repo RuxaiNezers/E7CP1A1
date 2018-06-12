509055e57e72317c959457e83b1c2d9ee9bb9911
RuxaiNezers <alexis.neira.cruz@gmail.com>
2018-06-12 19:34:53 -0400

    2.2 solved

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,6 @@ productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' =>
 
 productos.each { |producto, valor| puts "#{producto} = $#{valor}" }
 
+productos['cereal'] = 2200
+
+productos.each { |producto, valor| puts "#{producto} = $#{valor}" }
